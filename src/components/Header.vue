5b78b74d7fdd30e6a5f6fd75c79d45b4c9c3180b
Hua_Nan <x565619@gmail.com>
2021-09-11 10:00:35 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- Header -->
+  <header id="header" class="bg-gray-700">
+    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+      <!-- App Name -->
+      <router-link class="text-white font-bold uppercase text-2xl mr-4"
+      :to="{ name: 'home' }" exact-active-class="no-active">
+        Music
+        </router-link>
+
+      <div class="flex flex-grow items-center">
+        <!-- Primary Navigation -->
+        <ul class="flex flex-row mt-1">
+          <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
+          <li>
+            <a
+              class="px-2 text-white"
+              href="#"
+              @click.prevent="toggleAuthModal"
+            >
+              Login / Register
+            </a>
+          </li>
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  name: 'Header',
+  methods: {
+    ...mapMutations(['toggleAuthModal']),
+    // toggleAuthModal() {
+    //   // this.$store.state.authModalShow = !this.$store.state.authModalShow;
+    //   // console.log(this.$store.state.authModalShow);
+    //   // this.$store.commit('toggleAuthModal');
+    // },
+  },
+};
+</script>
---
+++ src/includes/firebase.js
@@ -0,0 +1,21 @@
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+// import 'firebase/firestore';
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBt6i9MCpGA0rhX7Q-LBqN_1Pq0DfLvKb4',
+//   authDomain: 'music-e6823.firebaseapp.com',
+//   projectId: 'music-e6823',
+//   storageBucket: 'music-e6823.appspot.com',
+//   messagingSenderId: '281008066183',
+//   appId: '1:281008066183:web:6b7d77d48d255c864d3222',
+// };
+
+// // Initialize Firebase
+
+// initializeApp(firebaseConfig);
+// const auth = getAuth();
+
+// export {
+//   auth,
+// };
