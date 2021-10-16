89e866396d7d047468a80edc3eef76b803479d88
Hua_Nan <x565619@gmail.com>
2021-10-17 02:25:03 +0800

    + player

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import { createStore } from 'vuex';
+import { Howl } from 'howler';
 import { auth, usersCollection } from '@/includes/firebase';
+import helper from '@/includes/helper';
 
 export default createStore({
   state: {
     authModalShow: false,
     userLoggedIn: false,
     currentSong: {},
+    sound: {},
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%',
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -16,10 +22,25 @@ export default createStore({
     },
     newSong(state, payload) {
       state.currentSong = payload;
+      state.sound = new Howl({
+        src: [payload.url],
+        html5: true,
+      });
+    },
+    updatePosition(state) {
+      state.seek = helper.formatTime(state.sound.seek());
+      state.duration = helper.formatTime(state.sound.duration());
+      state.playerProgress = `${(state.sound.seek() / state.sound.duration()) * 100}%`;
     },
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
   },
   actions: {
     async register({ commit }, payload) {
@@ -53,8 +74,53 @@ export default createStore({
       await auth.signOut();
       commit('toggleAuth');
     },
-    async newSong({ commit }, payload) {
+    async newSong({ commit, state, dispatch }, payload) {
+      // 避免同時撥放多個音檔
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
       commit('newSong', payload);
+      state.sound.play();
+      // 監聽播放歌曲後調用更新進度條
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
+    },
+    async toggleAudio({ state }) {
+      // 判斷Howler是否已準備好
+      if (!state.sound.playing) {
+        return;
+      }
+      // 判斷是否在播放中
+      if (state.sound.playing()) {
+        state.sound.pause();
+      } else {
+        state.sound.play();
+      }
+    },
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      }
+    },
+    updateSeek({ state, dispatch }, payload) {
+      if (!state.sound.playing) {
+        return;
+      }
+      const { x, width } = payload.currentTarget.getBoundingClientRect();
+      const clickX = payload.clientX - x;
+      const percentage = clickX / width;
+      const seconds = state.sound.duration() * percentage;
+
+      state.sound.seek(seconds);
+      state.sound.once('seek', () => {
+        dispatch('progress');
+      });
     },
   },
   modules: {
